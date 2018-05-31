e5c47cd30b88f0497ac8119378e36f697a31075f
jakub <jakub.pyra.1@gmail.com>
2018-05-31 16:43:03 +0200

    Zmiana kolejki na rezerwacje

--- holo.c
+++ holo.c
@@ -11,15 +11,16 @@
 #define RELEASE_TUGBOATS_TAG 2
 #define REPLY_TAG 3
 #define GO_AHEAD 0
-#define IMBEFOREYOU 1
+#define IMBEFOREYOU 10
 
 // msg[0] = timestamp
 // msg[1] = rank
 // msg[2] = boatsReq
 
 int inQueue = 0;
-int holowniki = 15;
+int holowniki = 25;
 int position = -1;
+int reserved = 0;
 int rank = -1;
 int boatsReq = 7;
 long timestamp = -1;
@@ -32,28 +33,34 @@ void *recvFun()
 	MPI_Status status;
 	while(1) {
 		MPI_Recv(msg, MSG_SIZE, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		int reply[1];
+		long reply[1];
 		switch (status.MPI_TAG) {
-			case 0:
+			case REQUEST_TAG:
 				if (inQueue) {
 					if (timestamp < msg[0] || timestamp == msg[0] && rank < msg[1]) {
-						reply[0] = IMBEFOREYOU;
+						reply[0] = boatsReq;
 					}
+					else
+						reply[0] = GO_AHEAD;
 				}
 				else {
 					reply[0] = GO_AHEAD;
 				}
 				MPI_Send(reply, 1, MPI_LONG, status.MPI_SOURCE, REPLY_TAG, MPI_COMM_WORLD);
 				break;
-			case 1:
+			case TAKE_TUGBOATS_TAG:
 				holowniki -= msg[2];
-				if (inQueue) position -= 1;
+				if (inQueue) {
+					reserved -= msg[2];
+				}
 				break;
-			case 2:
+			case RELEASE_TUGBOATS_TAG:
 				holowniki += msg[2];
 				break;
-			case 3:
-				if (msg[0] == IMBEFOREYOU) position += 1;
+			case REPLY_TAG:
+				if (msg[0] != GO_AHEAD) {
+					reserved += msg[0];
+				}
 				repliesRemaining -= 1;
 				break;
 		}
@@ -72,11 +79,17 @@ int sendToAll(long * msg, int msg_size, int tag){
 
 int main(int argc, char **argv)
 {
-	int msg[MSG_SIZE];
 	MPI_Status status;
 	pthread_t tid;
 
-	MPI_Init(&argc, &argv);
+	// MPI_Init(&argc, &argv);
+	int provided;
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
+	printf("provided: %d, required: %d\n", provided, MPI_THREAD_SERIALIZED);
+	if (provided < MPI_THREAD_SERIALIZED) {
+		printf("MPI does not provide needed threading level\n");
+		return(-1);
+	}
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 
@@ -87,8 +100,8 @@ int main(int argc, char **argv)
 	clock_settime(CLOCK_MONOTONIC, &ts);
 	srand(time(NULL)+rank);
 
-	boatsReq = rand()%7 + 1;
-	printf("Statek %d, potrzebuje %d holownikow\n", rank, boatsReq);
+	boatsReq = rand()%7 + 2;
+	printf("[%d]: potrzebuje %d holownikow\n", rank, boatsReq);
 	pthread_create(&tid, NULL, recvFun, NULL);
 
 	while(1){
@@ -100,19 +113,16 @@ int main(int argc, char **argv)
 		long msg[2];
 		msg[0] = timestamp;
 		msg[1] = rank;
-		printf("%d chce wpłynąć do portu.\n", rank);
+		printf("[%d]: chce wpłynąć do portu. time: %ld\n", rank, timestamp);
 		repliesRemaining = size - 1;
-		position = 0;
+		reserved = 0;
 		sendToAll(msg, 2, REQUEST_TAG);
 		while(repliesRemaining != 0){
 			usleep(10000);
 		}
-		printf("%d : %d w kolejce\n", rank, position);
-		while(position > 0){
-			usleep(10000);
-		}
-		printf("%d : %d holownikow dostepnych\n", rank, holowniki);
-		while(holowniki < boatsReq){
+		printf("[%d]: %d wolnych, %d zarezerwowanych holowników\n", rank, holowniki,
+		reserved);
+		while(holowniki - reserved < boatsReq) {
 			usleep(10000);
 		}
 		inQueue = 0;
@@ -121,13 +131,14 @@ int main(int argc, char **argv)
 		cmsg[1] = rank;
 		cmsg[2] = boatsReq;
 		// wejscie do strefy krytycznej
+		printf("[%d]: wchodze do strefy krytycznej, zabieram %d holownikow\n", rank, boatsReq);
 		holowniki -= boatsReq;
 		sendToAll(cmsg, 3, TAKE_TUGBOATS_TAG);
 		sleep(5); // strefa krytyczna
 		sendToAll(cmsg, 3, RELEASE_TUGBOATS_TAG);
 		holowniki += boatsReq;
 
-		printf("%d, przepłynąłem\n", rank);
+		printf("[%d]: wyszedłem ze strefy krytycznej, oddaje %d holownikow\n", rank, boatsReq);
 	}
 
 	MPI_Finalize();
